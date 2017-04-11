e44241f84d979d5d7a3805250382e98196dddb8f
athornburg <alexthornburg1@gmail.com>
2017-04-10 22:00:41 -0500

    Renaming components dir to main

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { Hello } from "./components/hello";
+import { Hello } from "./main/hello";
 
 ReactDOM.render(
     <Hello/>,
