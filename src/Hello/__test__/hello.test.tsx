9ea7920dececcc52cf8d3986c0337126e6562bec
Rodolfo Sanchez <rodolfo2488@gmail.com>
2017-04-18 17:04:21 -0500

    dont mock it

--- src/Hello/__test__/hello.test.tsx
+++ src/Hello/__test__/hello.test.tsx
@@ -1,18 +1,20 @@
 import * as React from "react";
-// import * as TypeMoq from "typemoq";
 
 import {shallow, ShallowWrapper} from "enzyme";
 import {Hello, HelloProps} from "../Hello";
 
 describe("Hello Component", () => {
     let helloComponent: ShallowWrapper<Hello, any>;
-    // let props: TypeMoq.IMock<HelloProps> = TypeMoq.Mock.ofType<HelloProps>();
+    let props: HelloProps;
 
     beforeEach(() => {
-        // helloComponent = shallow(<Hello actions={props.actions} counter={props.counter} />);
+        props = {
+            counter: 2
+        } as HelloProps;
+        helloComponent = shallow(<Hello actions={props.actions} counter={props.counter} />);
     });
 
     it("says hello", () => {
-        // expect(helloComponent.find("h1").text()).toContain("Hello typescript and react!");
+        expect(helloComponent.find("h1").text()).toContain("Hello typescript and react!");
     });
 });
